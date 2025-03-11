6fac71e7000a467fa43e671070270db2137e538e
SumejaV <156528965+SumejaV@users.noreply.github.com>
2025-03-11 18:47:36 -0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,15 @@
 
 
 require('./bootstrap');
-
+// Inicializohet Vue.js dhe është i gatshëm për përdorim
 window.Vue = require('vue');
 
-
-
+// Regjistrohet një komponent i quajtur 'example-component' që mund të përdoret në shabllone Vue
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
+
+//Krijohet një instancë e re Vue dhe lidhet me elementin HTML me id "app"
+// Ky është aplikacioni Vue që do të menaxhojë ndërfaqen e përdoruesit
 const app = new Vue({
     el: '#app'
     
