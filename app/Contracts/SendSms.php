7c3198a34f50974b0ff6d41d6e4e9ebf55a24862
SumejaV <156528965+SumejaV@users.noreply.github.com>
2025-03-13 21:40:08 +0100

    Update SendSms.php

--- app/Contracts/SendSms.php
+++ app/Contracts/SendSms.php
@@ -1,9 +1,23 @@
 <?php
 
-namespace App\Contracts;
+namespace App\Contracts; // Deklarimi i hapësirës së emrit (namespace)
 
+/**
+ * Interface për dërgimin e mesazheve SMS.
+ * 
+ * Çdo klasë që implementon këtë interface duhet të zbatojë metodën `send()`.
+ */
 interface SendSms {
    
-
-     public function send($to, $from, $text, $template_id);
+    /**
+     * Metoda për dërgimin e një SMS-i.
+     *
+     * @param string $to Marrësi i mesazhit (numri i telefonit).
+     * @param string $from Dërguesi i mesazhit (mund të jetë një emër ose numër).
+     * @param string $text Përmbajtja e mesazhit.
+     * @param int|string $template_id Opsionale – ID e një template-i nëse përdoret një shabllon.
+     * 
+     * @return mixed Mund të kthejë një përgjigje të API-së ose një status të dërgimit.
+     */
+    public function send($to, $from, $text, $template_id);
 }
