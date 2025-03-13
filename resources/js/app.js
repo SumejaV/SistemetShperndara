2881fb900447e67a01e90ba5eea50b8afd73666b
SumejaV <156528965+SumejaV@users.noreply.github.com>
2025-03-13 21:33:41 +0100

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 
 
-
+// kerkohet skedari i bootstrap-it qe permban ngarkimin e skedareve te nevojshem dhe konfigurimin fillestar
 require('./bootstrap');
-
+// inicializohet Vue.js dhe është i gatshem per perdorim
 window.Vue = require('vue');
 
 
-
+// Regjistrohet nje komponent i quajtur 'examble-component' qe mund te perdoret ne shabllone Vue
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
